8812b10bb838afa3b1fe7939a0aff36b1ef8396c
Teti <rohatetiana@gmail.com>
2025-07-23 17:52:25 +0200

    styled preloader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-import Image from 'next/image';
 import Preloader from '@/components/templates/Preloader/Preloader';
+import Image from 'next/image';
 export default function Home() {
-  return (
-    <Preloader />
-  );
+  return <Preloader />;
 }
