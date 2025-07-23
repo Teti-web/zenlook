65dd68e38e60c2c98a0def55d9c76e1c24ef2800
Teti <rohatetiana@gmail.com>
2025-07-23 16:01:54 +0200

    draft preloader

---
+++ src/components/templates/Preloader/Preloader.tsx
@@ -0,0 +1,11 @@
+
+
+const Preloader = () => {
+  return (
+    <section className="w-full h-screen bg-brand flex flex-col items-start justify-end p-2.5 md:p-4">
+        <span className="italic text-4xl md:text-[45px] text-link font-regular">01%</span>
+    </section>
+  )
+}
+
+export default Preloader
